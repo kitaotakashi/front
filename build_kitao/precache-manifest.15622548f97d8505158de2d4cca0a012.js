15a16484bd43f52bcf45521f76ba044aee36f3f1
Takashi Kitao <kitao.takashi@ist.osaka-u.ac.jp>
2020-12-26 01:13:14 +0900

    kitao

---
+++ build_kitao/precache-manifest.15622548f97d8505158de2d4cca0a012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538ab00fc8425dd0cc5633bfe95a05ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49961e3aa9b25b9911eb",
+    "url": "/static/js/2.126063cd.chunk.js"
+  },
+  {
+    "revision": "728662538595f08f733af14019f4ae29",
+    "url": "/static/js/2.126063cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe3fe3c3769ba5ee6cd",
+    "url": "/static/js/main.09c3378a.chunk.js"
+  },
+  {
+    "revision": "df79543f183d7271c85a",
+    "url": "/static/js/runtime-main.333cfcd7.js"
+  }
+]);
